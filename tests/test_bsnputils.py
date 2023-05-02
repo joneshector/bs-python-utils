9e83e7782b2229ceaec6f89d1c279dbeb6f1fd34
Bernard Salanie <bs2237@columbia.edu>
2023-05-02 14:54:33 -0300

    added tests for new numpy routines

--- tests/test_bsnputils.py
+++ tests/test_bsnputils.py
@@ -18,9 +18,33 @@ from bs_python_utils.bsnputils import (
     nppow,
     nprepeat_col,
     nprepeat_row,
+    print_quantiles,
     rice_stderr,
 )
 from bs_python_utils.bsutils import bsexp, bslog
+
+
+def test_print_quantiles_vec():
+    v = np.arange(101.0)
+    q = np.arange(10.0) / 10.0
+    qvals = print_quantiles(v, q)
+    qvals_expected = np.arange(start=0.0, stop=100.0, step=10.0)
+    assert np.allclose(qvals, qvals_expected)
+
+
+def test_print_quantiles_list():
+    v1 = np.arange(101.0)
+    v2 = np.arange(101.0, 202.0)
+    lst = [v1, v2]
+    q = np.arange(10.0) / 10.0
+    qvals = print_quantiles(lst, q)
+    qvals_expected = np.column_stack(
+        (
+            np.arange(start=0.0, stop=100.0, step=10.0),
+            np.arange(start=101.0, stop=200.0, step=10.0),
+        )
+    )
+    assert np.allclose(qvals, qvals_expected)
 
 
 def test_bs_sqrt_pdmatrix():
